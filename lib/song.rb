e910d54f28d35d19a0ffe17b735c021291b03ba5
Makeda P Mckune <makedamckune@gmail.com>
2019-11-22 02:29:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,16 @@
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-
-end
+class Song 
+  attr_accessor :name, :artist_name 
+  @@ll = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save 
+    self.class.all << self 
+  end 
+  
+  def self.create
+    create << @@all 
+  end 
+  
